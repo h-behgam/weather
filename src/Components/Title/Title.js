ce95b0ce7b32f2f8d275ca1d5890d90448552ef0
h-behgam <h.behgam@gmail.com>
2023-12-04 09:42:16 +0100

    Update Title.js

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Title() {
   return (
-    <h1 className="text-center text-5xl pt-10 text-black">Weather</h1>
+    <h1 className="text-center text-5xl pt-3 sm:pt-10 text-white">Weather</h1>
   )
 }
 
